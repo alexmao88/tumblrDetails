30e5c6b5cae0eb928932503b0cbd0ec066aab2eb
Alex Mao <alexmao88@gmail.com>
2018-02-06 15:03:46 -0800

    everything fixed

--- tumblrFeed2/View Controllers/PhotosViewController.swift
+++ tumblrFeed2/View Controllers/PhotosViewController.swift
@@ -23,6 +23,8 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         
         tableView.delegate = self
         tableView.dataSource = self
+        self.becomeFirstResponder()
+        self.tableView.rowHeight = 200
         
         // Network request snippet
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
@@ -36,13 +38,13 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
                 print(dataDictionary)
                 
                 /* Get the posts and store in posts property */
-                
                 // Get the dictionary from the response key
                 let responseDictionary = dataDictionary["response"] as! [String: Any]
                 // Store the returned array of dictionaries in our posts property
                 self.posts = responseDictionary["posts"] as! [[String: Any]]
                 
                 /* Reload the table view */
+                self.tableView.reloadData()
             }
         }
         task.resume()
@@ -58,23 +60,18 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "This is row \(indexPath.row)"
-        
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = UITableViewCell()
+//        cell.textLabel?.text = "This is row \(indexPath.row)"
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell") as! PhotoCell
-        
+
         // Configure YourCustomCell using the outlets that you've defined.
-        
+
         let post = posts[indexPath.row]
-        
+
         if let photos = post["photos"] as? [[String: Any]] {
             // photos is NOT nil, we can use it!
-            
+
             /* Get the photo url */
             // 1.
             let photo = photos[0]
@@ -84,13 +81,39 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
             let urlString = originalSize["url"] as! String
             // 4.
             let url = URL(string: urlString)
-            
+
             cell.photoImageView.af_setImage(withURL: url!)
-            self.tableView.reloadData()
         }
         
         return cell
     }
+    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell") as! PhotoCell
+//
+//        // Configure YourCustomCell using the outlets that you've defined.
+//
+//        let post = posts[indexPath.row]
+//
+//        if let photos = post["photos"] as? [[String: Any]] {
+//            // photos is NOT nil, we can use it!
+//
+//            /* Get the photo url */
+//            // 1.
+//            let photo = photos[0]
+//            // 2.
+//            let originalSize = photo["original_size"] as! [String: Any]
+//            // 3.
+//            let urlString = originalSize["url"] as! String
+//            // 4.
+//            let url = URL(string: urlString)
+//
+//            cell.photoImageView.af_setImage(withURL: url!)
+//        }
+//
+//        return cell
+//    }
 
     /*
     // MARK: - Navigation
